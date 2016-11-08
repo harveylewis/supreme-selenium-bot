d2090faef6ba4887bdf4102688f611cda5cd8ac6
harveylewis <harvey.l@hotmail.co.uk>
2016-11-08 11:25:51 +0000

    Update SupATC.py

--- SupATC.py
+++ SupATC.py
@@ -4,7 +4,6 @@
 #
 #
 
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,25 +14,25 @@ pageRequest = requests.get('http://www.supremenewyork.com/shop/all/shirts')
 soup = BeautifulSoup(pageRequest.content, "html.parser")
 links = soup.select("div.turbolink_scroller a")
 
-
 allProductInfo = soup.find_all("a", class_="name-link")
-print allProductInfo
+# print allProductInfo
 
-linksList1 = []
+hrefLinks = []
 for href in allProductInfo:
-    linksList1.append(href.get('href'))
+    hrefLinks.append(href.get('href'))
 
-print(linksList1)
+itemLinks = set(hrefLinks)
+print itemLinks
 
 #Follow links and parse info
-for url in linksList1:
+for url in itemLinks:
 	print ('http://www.supremenewyork.com' + url)
 	pageRequest2 = requests.get('http://www.supremenewyork.com' + url)
 	soup2 = BeautifulSoup(pageRequest2.content, "html.parser")
 	itemName = soup2.find_all(itemprop="name")
-	#print(itemName)
+	print(itemName)
 	itemColour = soup2.find_all(class_="style")
-	#print(itemColour)
+	print(itemColour)
 
 
 
@@ -42,10 +41,10 @@ for url in linksList1:
 
 
 
-#linksList1 = []
+#hrefLinks = []
 #for link in 
-#linksList1.append([productInfo])
-#print linksList1
+#hrefLinks.append([productInfo])
+#print hrefLinks
 
 #refresh until new links are added
 #open the new links page
